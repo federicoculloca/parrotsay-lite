0209c3d54e0e43086fbf9e0963fe614aabbebef1
Federico Culloca <federicoculloca@users.noreply.github.com>
2018-10-29 10:09:05 +0100

    copyright notice on source file
    Because, apparently, it's important

--- parrotsay.c
+++ parrotsay.c
@@ -1,4 +1,4 @@
-/*
+/*  Copyright (C) 2018 Federico klez Culloca
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
